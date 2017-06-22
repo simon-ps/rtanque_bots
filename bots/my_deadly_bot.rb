2b4e579aa80caac65b133cb0ca3bb156ac91315c
Simon Antliff <simon.antliff@powershop.co.nz>
2017-06-22 14:10:26 +1200

    added racetrack navigation

--- bots/my_deadly_bot.rb
+++ bots/my_deadly_bot.rb
@@ -2,13 +2,23 @@ class MyDeadlyBot < RTanque::Bot::Brain
   NAME = 'my_deadly_bot'
   include RTanque::Bot::BrainHelper
 
-  TURRET_FIRE_RANGE = RTanque::Heading::ONE_DEGREE * 5.0
+  INTERVAL = 10000
 
-  FIRE_POWER = MIN_FIRE_POWER
+  TURRET_FIRE_RANGE = RTanque::Heading::ONE_DEGREE
+
+  FIRE_POWER = MAX_FIRE_POWER
+
+  def initialize(arena)
+    aw = arena.width
+    ah = arena.height
+
+    @positions = [[aw*0.2, ah*0.2], [aw*0.2, ah*0.8], [aw*0.8, ah*0.8], [aw*0.8, ah*0.2]]
+    @position ||= 0
+    super(arena)
+  end
 
   def tick!
     @target ||= nil
-
     @target_old = @target
 
     # head right
@@ -23,8 +33,15 @@ class MyDeadlyBot < RTanque::Bot::Brain
       acquire_target
     end
 
+    point = RTanque::Point.new(*@positions[@position], self.arena)
+    move_to(point)
+
+    if self.sensors.position.within_radius?(point, 20)
+      @position = (@position + 1) % @positions.size
+    end
+
     # every 100 ticks, log sensor info
-    at_tick_interval(100) do
+    at_tick_interval(INTERVAL) do
       puts "Tick ##{sensors.ticks}!"
       puts " Gun Energy: #{sensors.gun_energy}"
       puts " Health: #{sensors.health}"
@@ -40,11 +57,17 @@ class MyDeadlyBot < RTanque::Bot::Brain
         puts "  #{reflection.inspect}#{reflection.name} #{reflection.heading.inspect} #{reflection.distance}"
       end
     end
+
+    @my_old_pos = sensors.position
+  end
+
+  def move_to(point)
+    command.heading = self.sensors.position.heading(point)
+    command.speed = [self.sensors.position.distance(point) / 10, 1].max
   end
 
   def acquire_target
     command.radar_heading = sensors.radar_heading + MAX_RADAR_ROTATION
-
   end
 
   def get_radar_lock
@@ -56,15 +79,15 @@ class MyDeadlyBot < RTanque::Bot::Brain
   end
 
   def point_turret_at_target
-    return unless @target_old
+    return unless @target_old && @my_old_pos
 
     # compensate for targets movement
-    old_target_point = point_at(@target_old)
-    new_target_point = point_at(@target)
+    old_target_point = point_at(@my_old_pos, @target_old)
+    new_target_point = point_at(sensors.position, @target)
 
     target_heading = old_target_point.heading(new_target_point)
 
-    at_tick_interval(100) do
+    at_tick_interval(INTERVAL) do
       puts "old_target_point: #{old_target_point}, new_target_point: #{new_target_point}"
     end
 
@@ -76,11 +99,11 @@ class MyDeadlyBot < RTanque::Bot::Brain
     while sensors.position.distance(my_shot_pos) < sensors.position.distance(target_pos)
       estimated_ticks += 1
       target_pos = new_target_point.move(target_heading, target_speed * estimated_ticks)
-      my_shot_pos = sensors.position.move(sensors.position.heading(target_pos), RTanque::Shell.speed(FIRE_POWER) * estimated_ticks)
+      my_shot_pos = sensors.position.move(sensors.position.heading(target_pos), 5 + RTanque::Shell.speed(FIRE_POWER) * estimated_ticks)
       #break if my_shot_pos.on_wall? || target_pos.on_wall?
-      break if estimated_ticks > 200
+      break if estimated_ticks > 2000
 
-      at_tick_interval(100) do
+      at_tick_interval(INTERVAL) do
         puts "my_shot_pos: #{my_shot_pos}, target_pos: #{target_pos}"
       end
     end
@@ -94,7 +117,7 @@ class MyDeadlyBot < RTanque::Bot::Brain
 
     @leading_heading = sensors.position.heading(new_target_point)
 
-    at_tick_interval(100) do
+    at_tick_interval(INTERVAL) do
       puts "Speed: #{target_speed}, Ticks: #{estimated_ticks}, Target heading: #{@target.heading.to_degrees}, New: #{new_target_point}, Leading Heading: #{@leading_heading.to_degrees}"
     end
 
@@ -108,8 +131,8 @@ class MyDeadlyBot < RTanque::Bot::Brain
     end
   end
 
-  def point_at(reflection)
-    sensors.position.move(reflection.heading, reflection.distance)
+  def point_at(from, reflection)
+    from.move(reflection.heading, reflection.distance)
   end
 
   def oldtick!
